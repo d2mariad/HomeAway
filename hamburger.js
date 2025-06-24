b918264ab8094a4220cad4edd9e006e607ae6425
d2mariad <marisdia237@yahoo.com>
2025-06-25 01:29:05 +0300

    Clean upload for GitHub Pages

---
+++ hamburger.js
@@ -0,0 +1,17 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('nav');
+    
+    menuToggle.addEventListener('click', function() {
+        nav.classList.toggle('active');
+    });
+    
+    // Close menu when clicking on a nav link
+    const navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            nav.classList.remove('active');
+        });
+    });
+});
