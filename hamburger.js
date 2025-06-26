ff79218d16175c29dfb20a4284c89e76a368cadb
d2mariad <marisdia237@yahoo.com>
2025-06-26 20:08:21 +0300

    update la hero, culori si responsive

--- hamburger.js
+++ hamburger.js
@@ -15,3 +15,29 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+document.addEventListener('DOMContentLoaded', function () {
+  const modal = document.getElementById('detailsModal');
+  const airbnbBtn = document.getElementById('airbnbLink');
+  const bookingBtn = document.getElementById('bookingLink');
+  const closeBtn = document.querySelector('.close-btn');
+
+  document.querySelectorAll('.show-modal').forEach(button => {
+    button.addEventListener('click', function () {
+      const airbnbUrl = this.dataset.airbnb;
+      const bookingUrl = this.dataset.booking;
+      airbnbBtn.href = airbnbUrl;
+      bookingBtn.href = bookingUrl;
+      modal.style.display = 'flex';
+    });
+  });
+
+  closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
+
+  window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
+});
